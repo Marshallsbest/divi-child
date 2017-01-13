8f64e6aacb3bffffde8fc8f6b18d5cf255274383
Luke Marshall <luke@themarshallchronicles.com>
2017-01-13 09:09:54 -0500

    initial commit with the materialdesign  already in the file still untested

---
+++ functions.php
@@ -0,0 +1,20 @@
+<?php 
+
+add_action( 'wp_enqueue_scripts', 'my_enqueue_assets' ); 
+
+function my_enqueue_assets() { 
+
+    wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
+
+    wp_enqueue_style( 'ripple-style', get_template_directory_uri().'/bower_components/css-ripple-effect/dist/ripple.min.css' );
+
+    wp_enqueue_style( 'material-css', get_template_directory_uri().'/mdl/material.min.css' );
+
+    wp_enqueue_script( 'material-js', get_template_directory_uri().'/mdl/material.min.js' );
+
+} add_filter( 'wp_revisions_to_keep', 'divi_limit_revisions', 10, 2 );
+
+function divi_limit_revisions( $num ) {
+    $num = 3;
+    return $num;
+}
