bfbf89b9f2dcfd5c5aa37828edcef1e9bf93e767
Luke Marshall <luke@themarshallchronicles.com>
2017-01-14 02:49:36 -0500

    initial  commit

--- functions.php
+++ functions.php
@@ -1,14 +1,17 @@
 <?php 
 
-add_action( 'wp_enqueue_scripts', 'my_enqueue_assets' ); 
+add_action( 'wp_enqueue_scripts', 'my_enqueue_assets' );
 
-function my_enqueue_assets() { 
+/**
+ *
+ */
+function my_enqueue_assets() {
 
     wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
 
-    wp_enqueue_style( 'ripple-style', get_template_directory_uri().'/bower_components/css-ripple-effect/dist/ripple.min.css' );
-
     wp_enqueue_style( 'material-css', get_template_directory_uri().'/mdl/material.min.css' );
+
+    wp_enqueue_script( 'material-js', get_template_directory_uri().'/mdl/material.min.js' );
 
     wp_enqueue_script( 'material-js', get_template_directory_uri().'/mdl/material.min.js' );
 
